9b4d2399dda7a346240329584511aa891725601f
Banashbib <Banashbib123@gmail.com>
2023-10-08 14:41:37 +0300

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Bana Alshabib on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Bana Alshabib on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
